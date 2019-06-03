0e5f12754fcdf2834c478b597a19c7b2b04bbd11
tomyeh <tomyeh@potix.com>
2019-06-03 18:27:00 +0800

    Attachment and Recurring

--- src/main/java/io/quire/api/model/Attachment.java
+++ src/main/java/io/quire/api/model/Attachment.java
@@ -4,17 +4,27 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel()
-public class Attachment extends StampedEntity {
-    @ApiModelProperty(example = "file.zip")
-    public String getName() {
-        return null;
-    }
+public class Attachment {
+    @ApiModelProperty(example = "2048",
+        value = "The type of this attachment."
+        + "It is 1 if it is from Google Drive. "
+        + "It is 2 if it is stored in Quire.")
+    public int getType() { return 0; }
 
-    @ApiModelProperty(example = "https://quire.io/att/Ta/sdcQOGgeUtyaFFzb9p0IwAgi/qfqVmUtC/image.png")
-    public String getUrl() {
-        return null;
-    }
+    @ApiModelProperty(example = "file.zip",
+        value = "Attachment's name.")
+    public String getName() { return null; }
 
-    @ApiModelProperty(example = "2048")
-    public Integer getLength() { return null; }
+    @ApiModelProperty(example = "https://quire.io/att/Ta/sdcQOGgeUtyaFFzb9p0IwAgi/qfqVmUtC/image.png",
+        value = "URL of this attachment.")
+    public String getUrl() { return null; }
+
+    @ApiModelProperty(example = "2048",
+        value = "The size of this attachment. Unit: bytes.")
+    public int getLength() { return 0; }
+
+    @ApiModelProperty(value = "When this record was created.", example = "2018-12-22T02:06:58.158Z", position = 99)
+    public String getCreatedAt() { return null; }
+    @ApiModelProperty(value = "OID of the user who created this record.", example = "Dyh2YkFcu9uLgLFIeN1kB4Ld", position = 99)
+    public String getCreatedBy() { return null; }
 }
--- src/main/java/io/quire/api/model/Recurring.java
+++ src/main/java/io/quire/api/model/Recurring.java
@@ -5,5 +5,29 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel()
 public class Recurring {
-	//TODO
+	@ApiModelProperty(example = "2048",
+		value = "The type of this recurring."
+		+ "It is 0 if it is weekly. "
+		+ "It is 1 if it is monthly. "
+		+ "It is 2 if it is yearly. "
+		+ "It is 3 if it is custom. ")
+    public int getType() { return 0; }
+
+	@ApiModelProperty(example = "2048",
+		value = "How often this recurring shall occur. "
+		+ "If the rate is 2 and the type is weekly, it means it shall "
+		+ "occur every two week. "
+		+ "If the type is custom, it means number of days to repeat.")
+	public int getRate() { return 0; }
+
+	@ApiModelProperty(value = "When this recurring shall end. "
+		+ "If not specified, it means it is never end.",
+		example = "2020-12-22T00:00:00.000Z", position = 4)
+	public String getEnd() { return null; }
+
+	@ApiModelProperty(
+		value = "It depends on the type of this recurring. "
+		+ "If weekly, bit 0 is Sunday, bit 1 is Monday and so on. "
+   		+ "For example, if the data is 6, it means every Monday and Tuesday.")
+	public int getData() { return 0; }
 }
