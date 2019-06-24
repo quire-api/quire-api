59a6d4bc910367f45d55a812e7c06b158a21c82d
tomyeh <tomyeh@potix.com>
2019-06-24 15:29:13 +0800

    doc

--- src/main/java/io/quire/api/model/project/UpdateProject.java
+++ src/main/java/io/quire/api/model/project/UpdateProject.java
@@ -8,14 +8,20 @@ import io.swagger.annotations.ApiModelProperty;
 public class UpdateProjectBody {
 	@ApiModelProperty(example = "[\"6QMKkEPBVWETLWrXqws94ALU\",\"app\"]",
 		value = "(Optional) OID of the users to replace the followers of this project. "
-		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.")
+		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.\n"
+		+ "In additions, it can pass additional information in the syntax of "
+		+ "\"app|team\" or \"app|team|channel\" where team and channel can be any value.")
 	public List<String> getFollowers() { return null; }
 	@ApiModelProperty(example = "[\"6QMKkEPBVWETLWrXqws94ALU\"]",
 		value = "(Optional) OID of the followers to be added to this project."
-		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.")
+		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.\n"
+		+ "In additions, it can pass additional information in the syntax of "
+		+ "\"app|team\" or \"app|team|channel\" where team and channel can be any value.")
 	public List<String> getAddFollowers() { return null; }
 	@ApiModelProperty(example = "[\"6QMKkEPBVWETLWrXqws94ALU\"]",
 		value = "(Optional) OID of the followers to be removed from this project."
-		+ "If the application won't like to follow any more, it can pass \"app\" as one of OIDs.")
+		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.\n"
+		+ "In additions, it can pass additional information in the syntax of "
+		+ "\"app|team\" or \"app|team|channel\" where team and channel can be any value.")
 	public List<String> getRemoveFollowers() { return null; }
 }
--- src/main/java/io/quire/api/model/task/UpdateTaskBody.java
+++ src/main/java/io/quire/api/model/task/UpdateTaskBody.java
@@ -71,14 +71,20 @@ public class UpdateTaskBody {
 
 	@ApiModelProperty(example = "[\"6QMKkEPBVWETLWrXqws94ALU\",\"app\"]",
 		value = "(Optional) OID of the users to replace the followers of this task. "
-		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.")
+		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.\n"
+		+ "In additions, it can pass additional information in the syntax of "
+		+ "\"app|team\" or \"app|team|channel\" where team and channel can be any value.")
 	public List<String> getFollowers() { return null; }
 	@ApiModelProperty(example = "[\"6QMKkEPBVWETLWrXqws94ALU\"]",
 		value = "(Optional) OID of the followers to be added to this task."
-		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.")
+		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.\n"
+		+ "In additions, it can pass additional information in the syntax of "
+		+ "\"app|team\" or \"app|team|channel\" where team and channel can be any value.")
 	public List<String> getAddFollowers() { return null; }
 	@ApiModelProperty(example = "[\"6QMKkEPBVWETLWrXqws94ALU\"]",
 		value = "(Optional) OID of the followers to be removed from this task."
-		+ "If the application won't like to follow any more, it can pass \"app\" as one of OIDs.")
+		+ "If the application would like to follow, it can pass \"app\" as one of OIDs.\n"
+		+ "In additions, it can pass additional information in the syntax of "
+		+ "\"app|team\" or \"app|team|channel\" where team and channel can be any value.")
 	public List<String> getRemoveFollowers() { return null; }
 }
