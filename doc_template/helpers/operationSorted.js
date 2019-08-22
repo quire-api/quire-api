a0f7165eb215ce72157966cc340515402bfaef4a
jimmyshiau <jimmyshiau73@gmail.com>
2019-08-22 15:21:17 +0800

    [API doc] Adjust operation order

--- doc_template/helpers/operationSorted.js
+++ doc_template/helpers/operationSorted.js
@@ -2,19 +2,26 @@ var Handlebars = require('handlebars')
 
 module.exports = function(context, options) {
   var ret = "";
-  var order = ['post','get','put','delete'];
+  var methodOrder = ['post','get','put','delete'];
+  var pathOrder = ['id','{oid}','list', 'search', 'after', 'before'];
   var data;
   if (typeof context !== "object") {
     return ret;
   }
   var keys = Object.keys(context)
   keys.sort(function(a,b) {
-    a = order.indexOf(context[a].method)
-    b = order.indexOf(context[b].method)
-    if (a == b) return 0;
-    if (a > b) return 1;
-    return -1;
+    var aMethod = methodOrder.indexOf(context[a].method)
+    var bMethod = methodOrder.indexOf(context[b].method)
+    if (aMethod == bMethod) {
+        var aPath = pathOrder.indexOf(context[a].path.split("/")[2])
+        var bPath = pathOrder.indexOf(context[b].path.split("/")[2])
+
+        return aPath - bPath;
+    }
+
+    return aMethod - bMethod;
   }).forEach(function(key, index) {
+    //console.log(context[key].method + ', ' + context[key].path);
     if (options.data) {
       data = Handlebars.createFrame(options.data || {})
       data.index = index;
