0c0ecf1f6ce40f3fe53a0ae2f6ed7a0741a1d76d
Tom Yeh <tomyeh@potix.com>
2022-11-15 15:10:26 +0800

    Fix doc error

--- src/main/java/io/quire/api/model/sublist/CreateSublistBody.java
+++ src/main/java/io/quire/api/model/sublist/CreateSublistBody.java
@@ -44,7 +44,7 @@ public class CreateSublistBody {
 
 	@ApiModelProperty(value = "(Optional) A list of tasks' OID that belong to this sublist.\n"
 		+ "Note: all of descendants will be included too.")
-	public List<String> getTasks() { return null; }
+	public List<String> getIncludes() { return null; }
 
 	@ApiModelProperty(value = "(Optional) OID of the external team that this sublist belongs to.",
 		example = "rcBHBYXZSiyDRrHrWPutatfF", position = 15)
