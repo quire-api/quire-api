f52786f6ce1e2ebd146bdc8ccf025435c70f3f5a
Tom Yeh <tomyeh@potix.com>
2024-03-14 18:41:48 +0800

    Sublist supports organization, folder and smart-folder

--- src/main/java/io/quire/api/resource/SublistResource.java
+++ src/main/java/io/quire/api/resource/SublistResource.java
@@ -14,26 +14,38 @@ import javax.ws.rs.core.Response;
 @Produces({"application/json"})
 public class SublistResource {
 	@POST
-	@Path("/{projectOid}")
-	@ApiOperation(value = "Add a new sublist.",
-		notes = "Add a new sublist into a project.",
+	@Path("/{ownerType}/{ownerOid}")
+	@ApiOperation(value = "Add a new sublist by owner's OID.",
+		notes = "Add a new sublist by owner's OID",
 		response = Sublist.class)
 	public Response createSublist(
-		@ApiParam(value = "OID of project that this new sublist to be added to.",
+		@ApiParam(value = "The type of the owner that this new sublist to be added to. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/this_is_oid` is "
+		+ "equivalent to `/project/this_is_oid`.",
+		required = false)
+		@PathParam("ownerType") String ownerType,
+		@ApiParam(value = "OID of the owner that this new sublist to be added to.",
 		required = true)
-		@PathParam("projectOid") String projectOid,
+		@PathParam("ownerOid") String ownerOid,
 		@ApiParam(value = "Sublist to create", required = true)
 		CreateSublistBody data) { return null; }
 
 	@POST
-	@Path("/id/{projectId}")
-	@ApiOperation(value = "Add a new sublist.",
-		notes = "Add a new sublist into a project.",
+	@Path("/id/{ownerType}/{ownerId}")
+	@ApiOperation(value = "Add a new sublist by owner's ID.",
+		notes = "Add a new sublist by owner's ID.",
 		response = Sublist.class)
 	public Response createSublistToProject(
-		@ApiParam(value = "ID of project that this new sublist to be added to.",
+		@ApiParam(value = "The type of the owner that this new sublist to be added to. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/id/this_is_id` is "
+		+ "equivalent to `/id/project/this_is_id`.",
+		required = false)
+		@PathParam("owenerType") String ownerType,
+		@ApiParam(value = "ID of owner that this new sublist to be added to.",
 		required = true)
-		@PathParam("projectId") String projectId,
+		@PathParam("ownerId") String ownerId,
 		@ApiParam(value = "Sublist to create", required = true)
 		CreateSublistBody data) { return null; }
 
@@ -47,39 +59,58 @@ public class SublistResource {
 		@PathParam("oid") String oid) { return null; }
 
 	@GET
-	@Path("/id/{projectId}/{id}")
+	@Path("/id/{ownerType}/{ownerId}/{id}")
 	@ApiOperation(value = "Get an existing sublist by its ID.",
 		notes = "Returns the full sublist record of the given ID.",
 		response = Sublist.class)
 	public Response getSublistById(
-		@ApiParam(value = "ID of the project that the sublist belongs to.", required = true)
-		@PathParam("projectId") int projectId,
+		@ApiParam(value = "The type of the owner that this sublist belongs to. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/id/this_is_id` is "
+		+ "equivalent to `/id/project/this_is_id`.",
+		required = false)
+		@PathParam("owenerType") String ownerType,
+		@ApiParam(value = "ID of owner that this sublist belongs to.",
+		required = true)
+		@PathParam("ownerId") String ownerId,
 		@ApiParam(value = "ID of the sublist that needs to be fetched", required = true)
 		@PathParam("id") String id) { return null; }
 
 	@GET
-	@Path("/list/{projectOid}")
-	@ApiOperation(value = "Get all sublists of the given project by its OID.",
-		notes = "Returns all sublist records of the given project by its OID.",
+	@Path("/list/{ownerType}/{ownerOid}")
+	@ApiOperation(value = "Get all sublists of the given owner by its OID.",
+		notes = "Returns all sublist records of the given owner by its OID.",
 		response = Sublist.class,
 		responseContainer = "List")
 	public Response getSublistsByProjectOid(
-		@ApiParam(value = "OID of the project.", required = true)
-		@PathParam("projectOid") String projectOid) { return null; }
+		@ApiParam(value = "The type of the owner. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/list/this_is_oid` is "
+		+ "equivalent to `/list/project/this_is_oid`.",
+		required = false)
+		@PathParam("ownerType") String ownerType,
+		@ApiParam(value = "OID of the owner.", required = true)
+		@PathParam("ownerOid") String ownerOid) { return null; }
 
 	@GET
-	@Path("/list/id/{projectId}")
-	@ApiOperation(value = "Get all sublists of the given project by its ID.",
-		notes = "Returns all sublist records of the given project by its ID.",
+	@Path("/list/id/{ownerType}/{ownerId}")
+	@ApiOperation(value = "Get all sublists of the given owner by its ID.",
+		notes = "Returns all sublist records of the given owner by its ID.",
 		response = Sublist.class,
 		responseContainer = "List")
 	public Response getSublistsByProjectId(
-		@ApiParam(value = "ID of project.", required = true)
-		@PathParam("projectId") String projectId) { return null; }
+		@ApiParam(value = "The type of the owner. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/list/id/this_is_id` is "
+		+ "equivalent to `/list/id/project/this_is_id`.",
+		required = false)
+		@PathParam("ownerType") String ownerType,
+		@ApiParam(value = "ID of the owner.", required = true)
+		@PathParam("ownerId") String ownerId) { return null; }
 
 	@PUT
 	@Path("/{oid}")
-	@ApiOperation(value = "Update a sublist.",
+	@ApiOperation(value = "Update a sublist by its OID.",
 		notes = "Updates an existing sublist, and returns the complete updated record.",
 		response = Sublist.class)
 	public Response updateSublist(
@@ -89,21 +120,25 @@ public class SublistResource {
 		UpdateSublistBody data) { return null; }
 
 	@PUT
-	@Path("/id/{projectId}/{id}")
+	@Path("/id/{ownerType}/{ownerId}/{id}")
 	@ApiOperation(value = "Update a sublist by its ID.",
 		notes = "Updates an existing sublist, and returns the complete updated record.",
 		response = Sublist.class)
 	public Response updateSublist(
-		@ApiParam(value = "ID of the project that the sublist belongs to.", required = true)
-		@PathParam("projectId") int projectId,
-		@ApiParam(value = "ID of the sublist that needs to be fetched", required = true)
-		@PathParam("id") String id,
+		@ApiParam(value = "The type of the owner. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/id/this_is_id` is "
+		+ "equivalent to `/id/project/this_is_id`.",
+		required = false)
+		@PathParam("ownerType") String ownerType,
+		@ApiParam(value = "ID of the owner.", required = true)
+		@PathParam("ownerId") String ownerId,
 		@ApiParam(value = "Sublist to update", required = true)
 		UpdateSublistBody data) { return null; }
 
 	@DELETE
 	@Path("/{oid}")
-	@ApiOperation(value = "Delete a sublist",
+	@ApiOperation(value = "Delete a sublist by its OID",
 		notes = "Delete an existing sublist of the given OID.")
 	@ApiResponses(value = {
 		@ApiResponse(code = 200, message = "ok",
@@ -114,7 +149,7 @@ public class SublistResource {
 		@PathParam("oid") String oid) { return null; }
 
 	@DELETE
-	@Path("/id/{projectId}/{id}")
+	@Path("/id/{ownerType}/{ownerId}/{id}")
 	@ApiOperation(value = "Delete a sublist by its ID",
 		notes = "Delete an existing sublist of the given OID.")
 	@ApiResponses(value = {
@@ -122,8 +157,12 @@ public class SublistResource {
 			examples = @Example({@ExampleProperty(mediaType = "application/json", value =
 				"{'Success': 'true'}")}))})
 	public Response deleteSublist(
-		@ApiParam(value = "ID of the project that the sublist belongs to.", required = true)
-		@PathParam("projectId") int projectId,
-		@ApiParam(value = "ID of the sublist that needs to be fetched", required = true)
-		@PathParam("id") String id) { return null; }
+		@ApiParam(value = "The type of the owner. "
+		+ "It can be `project`, `organization`, `folder` or `smart-folder`. "
+		+ "If omitted, `project` is assumed. For example, `/id/this_is_id` is "
+		+ "equivalent to `/id/project/this_is_id`.",
+		required = false)
+		@PathParam("ownerType") String ownerType,
+		@ApiParam(value = "ID of the owner.", required = true)
+		@PathParam("ownerId") String ownerId) { return null; }
 }
