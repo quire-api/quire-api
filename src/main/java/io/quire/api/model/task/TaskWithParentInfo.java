2064baf9f6a9648ef5b835834d44e832d37f31a5
Whiterzi <catwhiter@gmail.com>
2026-02-13 14:27:42 +0800

    Fix compile error, replace semicolon with comma

--- src/main/java/io/quire/api/model/task/TaskWithParentInfo.java
+++ src/main/java/io/quire/api/model/task/TaskWithParentInfo.java
@@ -8,7 +8,7 @@ public class TaskWithParentInfo extends Task {
       value = "The parent information, if this task has a parent. "
       + "If this task has no parent, this field will be null.\n\n"
       + "Note: It returns at most 10 levels of parent tasks. "
-      + "If exceeded, the 11th item will contain only the id.\n\n";
+      + "If exceeded, the 11th item will contain only the id.\n\n",
       position = 80
   )
   public TaskInfo getParent() { return null; }
